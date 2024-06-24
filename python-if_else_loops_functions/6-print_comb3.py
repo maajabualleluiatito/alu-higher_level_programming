008a506aa73e896b12b1810a5876e051cd43bd18
maajabualleluiatito <m.tito@alustudent.com>
2024-06-24 13:20:21 +0200

    hjfvhjafv

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
-
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print('89')
+for i in range(0, 10):
+    for j in range((i+1), 10):
+        if (i is not 8) or (j is not 9):
+            print("{}{}, ".format(i, j), end="")
         else:
-            print('{}{}, '.format(x, y), end='')
+            print("{}{}".format(i, j)) 
