f9753b39ed2ac08b9bf07a3b3e7808791384d05d
maajabualleluiatito <m.tito@alustudent.com>
2024-06-27 09:23:51 +0200

    haudvuajd

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from add_0.py import def add(a, b):
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    c = a + b
-print("a",a + "b",b = c)
-"__main__" "__name__"
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
