2e694cd7c2e4221c91ccc706c3d405369291934a
maajabualleluiatito <m.tito@alustudent.com>
2024-07-11 15:54:16 +0200

    afvafvafv'

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -17,7 +17,36 @@ class MyList(list):
         """
         print(sorted(self))
 
+
 # Example usage
 if __name__ == "__main__":
-    my_list = MyList([3, 1, 4, 1, 5, 9, 2, 6, 5])
+    my_list = MyList()
+    print(my_list)  # Output: []
+    
+    my_list.append(3)
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(1)
+    my_list.append(5)
+    my_list.append(9)
+    my_list.append(2)
+    my_list.append(6)
+    my_list.append(5)
+    print(my_list)  # Output: [3, 1, 4, 1, 5, 9, 2, 6, 5]
+    
     my_list.print_sorted()  # Output: [1, 1, 2, 3, 4, 5, 5, 6, 9]
+    
+    my_list = MyList()
+    my_list.append(-3)
+    my_list.append(-1)
+    my_list.append(4)
+    my_list.append(-1)
+    my_list.append(5)
+    my_list.append(-9)
+    my_list.append(2)
+    my_list.append(-6)
+    my_list.append(5)
+    print(my_list)  # Output: [-3, -1, 4, -1, 5, -9, 2, -6, 5]
+    
+    my_list.print_sorted()  # Output: [-9, -6, -3, -1, -1, 2, 4, 5, 5]
+
