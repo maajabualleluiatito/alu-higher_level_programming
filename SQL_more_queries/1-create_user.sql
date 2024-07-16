4db6083fddf0b73fad3d3b0a156f260649ef8983
maajabualleluiatito <m.tito@alustudent.com>
2024-07-16 12:05:22 +0200

    avafvavvf

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,8 @@
--- creates the MySQL server user_0d_1 and grant all priviledges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+-- Creates the MySQL server user user_0d_1 if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grants all privileges to the user user_0d_1 on the MySQL server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Applies the privilege changes
+FLUSH PRIVILEGES;
