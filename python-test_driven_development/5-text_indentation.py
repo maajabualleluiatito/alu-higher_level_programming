71fe83f053c484bec36e5d01b8f299f2637a8be9
maajabualleluiatito <m.tito@alustudent.com>
2024-07-30 12:11:53 +0200

    hgjvabjvbasfv

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""Module documentation"""
+
+
+def text_indentation(text):
+    """function documentation"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
