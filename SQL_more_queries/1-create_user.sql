30741cfc9fc04f533453634595a1e5b179fc1295
maajabualleluiatito <m.tito@alustudent.com>
2024-07-31 15:24:20 +0200

    DDCdcDAC

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,6 @@
--- creates the MySQL server user user_0d_1
--- should have all privileges
--- password should be set to user_0d_1_pwd
--- if user_0d_1 already exists, script should not fail
-
+-- Creates a MySQL Server user `user_0d_1`
+-- and sets a temporary password `user_0d_1_pwd`.
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
