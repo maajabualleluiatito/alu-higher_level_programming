9b18d8d01715284343919ca20537a4ad5ec0513f
maajabualleluiatito <m.tito@alustudent.com>
2024-07-31 15:29:25 +0200

    afdvavasdv

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
--- Creates a MySQL Server user `user_0d_1`
--- and sets a temporary password `user_0d_1_pwd`.
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+-- creates the MySQL server user_0d_1 and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
