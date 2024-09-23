c66d746aee1fc11695c1874c696a2e8b1d0a70a4
maajabualleluiatito <m.tito@alustudent.com>
2024-09-23 12:30:30 +0200

    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
+  return (n === 0) ? 1 : n * factorial(n - 1);
+}
+
+const arg = parseInt(process.argv[2]);
+console.log(factorial(arg));
